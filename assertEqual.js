95fcb44b10db05fff7291e9778abce9013414a62
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-06-27 17:22:55 -0600

    initial commit

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  const fail = "🔴";
+  const pass = "✅";
+  if (actual !== expected) {
+    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }else {
+    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "BootCamp");
+assertEqual(1, 1);
