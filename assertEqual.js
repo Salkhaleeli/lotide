8d85d1684629785679bab100cc862a8b328100a5
vagrant <eng.saad.najm@gmail.com>
2022-06-27 23:38:28 +0000

    fixing string

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,11 @@ const assertEqual = function(actual, expected) {
   const fail = "🔴";
   const pass = "✅";
   if (actual !== expected) {
-    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`${fail}${fail}${fail} Assertion Failed:
+     ${actual} is not a ${expected}`);
   }else {
-    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
+    console.log(`${pass}${pass}${pass} Assertion Passed:
+     ${actual} = ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs!", "Bootcamp!");
