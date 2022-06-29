5e2d08013e63a6da4acaf7d0c7c3ae3c224178d5
vagrant <eng.saad.najm@gmail.com>
2022-06-29 02:38:34 +0000

    modifying

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,14 @@
-// const fail = "🔴"
-// const pass = "✅";
-// const eqArrays = function (Arr1, Arr2) {
-//   for (let i = 0; i < Arr1.length; i++) {
-//     if (Arr1[i] !== Arr2[i]) {
-//       return false;
-//     }
-//     return true;
-//   }
-// };
-
 const eqArrays = function(Arr1, Arr2) {
+  if (Arr1.length !== Arr2.length) {
+    return false
+  }
   for (let i = 0; i < Arr1.length; i++) {
     if (Arr1[i] !== Arr2[i]) {
-      return false
-    }else{
-      return true
+      return false  
     }
-    
   }
+  return true
 }
 console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+console.log(eqArrays([1,2,3], [1,2]));
