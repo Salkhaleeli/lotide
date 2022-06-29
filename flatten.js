d2bbbdb2143d4809246232d8bcb2539a2fb763c3
vagrant <eng.saad.najm@gmail.com>
2022-06-29 03:55:37 +0000

    initial commit

---
+++ flatten.js
@@ -0,0 +1,14 @@
+const flatten = function(arr) {
+  let flatArray = []
+  for (const iterator of arr) {
+    if (Array.isArray(iterator)) {
+      for (const iterator2 of iterator) {
+        flatArray.push(iterator2)
+      }
+    } else{
+      flatArray.push(iterator)
+    }
+  }
+  return flatArray
+}
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
