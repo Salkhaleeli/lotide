fcb79b79f3ae5f4b7aa7d8495a0622a5e1fc2cf2
vagrant <eng.saad.najm@gmail.com>
2022-07-04 20:39:39 +0000

    initial commit

---
+++ tail.js
@@ -0,0 +1,7 @@
+const assertEqual = require('./assertEqual');
+
+const tail = function(array) {
+  return array[array.length-1];
+};
+
+console.log(assertEqual(tail(["Hello", "Lighthouse", "Labs"]), 'Labs'));
